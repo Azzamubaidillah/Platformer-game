ed8bd36ba9790db76ff8595eee84f2612952db8e
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2020-09-22 20:20:33 +0700

    player 1

--- platformer/src/actors/actor.gd
+++ platformer/src/actors/actor.gd
@@ -1,6 +1,11 @@
 extends KinematicBody2D
 class_name actor
 
+export var speed: = Vector2(300.0, 1000.0)
+export var gravity: = 3000.0
+
+var velocity: = Vector2.ZERO
+
 func _physics_process(delta):
-	var velocity: = Vector2(300, 0)
-	move_and_slide()
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity)
