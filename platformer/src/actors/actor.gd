960e8798f4734cdaa5106fd17c0c411738629390
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2020-09-24 13:19:58 +0700

    add jump

--- platformer/src/actors/actor.gd
+++ platformer/src/actors/actor.gd
@@ -1,11 +1,12 @@
 extends KinematicBody2D
 class_name actor
 
+const FLOOR_NORMAL: = Vector2.UP
+
 export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 3000.0
 
 var velocity: = Vector2.ZERO
 
-func _physics_process(delta):
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity)
+
+
