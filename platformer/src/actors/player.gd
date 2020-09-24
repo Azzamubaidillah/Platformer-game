7df4130570a003559f676991152a74508be3a2e5
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2020-09-24 12:44:09 +0700

    move left

--- platformer/src/actors/player.gd
+++ platformer/src/actors/player.gd
@@ -1,2 +1,8 @@
-extends KinematicBody2D
+extends actor
 
+func _physics_process(delta: float) -> void:
+	var direction: = Vector2(
+		Input.get_action_strength("move_right") - 	Input.get_action_strength("move_left"),
+		0.0
+	)
+	velocity = speed * direction
