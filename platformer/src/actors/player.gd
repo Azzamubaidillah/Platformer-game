347b444358c6ba4f5be740326aa39713b3699bef
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2020-09-25 15:03:53 +0700

    replace new velocity to out and velocity to _velocity

--- platformer/src/actors/player.gd
+++ platformer/src/actors/player.gd
@@ -1,10 +1,10 @@
 extends actor
 
 func _physics_process(delta: float) -> void:
-	var is_jump_interrupted: = Input.is_action_just_released("jump") and velocity.y < 0.0
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
-	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
-	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 
 func get_direction() -> Vector2:
 	return Vector2(
@@ -18,11 +18,11 @@ func calculate_move_velocity(
 		speed: Vector2,
 		is_jump_interrupted: bool
 	) -> Vector2:
-	var new_velocity: = linear_velocity
-	new_velocity.x = speed.x * direction.x
-	new_velocity.y += gravity * get_process_delta_time()
+	var out: = linear_velocity
+	out.x = speed.x * direction.x
+	out.y += gravity * get_process_delta_time()
 	if direction.y == -1.0:
-		new_velocity.y = speed.y * direction.y
+		out.y = speed.y * direction.y
 	if is_jump_interrupted:
-		new_velocity.y = 0.0
-	return new_velocity
+		out.y = 0.0
+	return out
