cc39685208e5e7272b720c5e45aa8bc18cb36313
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2020-09-30 17:20:23 +0700

    fix portal

--- platformer/src/objects/Portal2D.gd
+++ platformer/src/objects/Portal2D.gd
@@ -1,18 +1,23 @@
 tool
 extends Area2D
 
+
 onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 export var next_scene: PackedScene
 
-func _on_body_entered(body):
+
+func _on_body_entered(body: PhysicsBody2D):
 	teleport()
 
-func _get_configuration_warning():
-	return "The next scene property can't be empty" if not next_scene else ""
+
+func _get_configuration_warning() -> String:
+	return "The property Next Level can't be empty" if not next_scene else ""
+
 
 func teleport() -> void:
-	anim_player.play("fade_in")
-	yield(anim_player, "animation_finished")	
-	get_tree().changed_scene_to(next_scene)
-	
+	get_tree().paused = true
+	anim_player.play("fade_out")
+	yield(anim_player, "animation_finished")
+	get_tree().paused = false
+	get_tree().change_scene_to(next_scene)
